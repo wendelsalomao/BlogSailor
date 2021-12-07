e0f8934de8b572d7fa5ae92b384595d6f5aa8e6f
wendelsalomao <wendelsalomao@outlook.com>
2021-12-07 20:30:45 -0300

    Update index.php
    Novo editado
    
    Co-Authored-By: wendelsalomao <61808789+wendelsalomao@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,19 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Blog Sailor</title>
 </head>
 <body>
     <nav>
-        <div class="logo"><h1>LogoMarca</h1></div>
+        <div class="logo"><h1>Blog Sailor</h1></div>
         <div class="openMenu"><i class="fa fa-bars"></i></div>
         <ul class="mainMenu">
-            <li>
-                <img src="./imagens/home.svg">
-                <a href="#">Home</a></li>
-            <li>
-                <img src="./imagens/about.svg" alt="">
-                <a href="#">Sobre</a></li>
             <li>
                 <img src="./imagens/logar.svg">
                 <a href="cadastro.php">Cadastrar</a></li>
@@ -40,19 +34,7 @@
         <div class="mySlides fade">
           <div class="numbertext">1 / 3</div>
           <img class="imgcarrosel" src="./imagens/slide1.jpg" style="width:100%">
-          <div class="text">Lorem ipsum 1 </div>
-        </div>
-        
-        <div class="mySlides fade">
-          <div class="numbertext">2 / 3</div>
-          <img class="imgcarrosel" src="./imagens/slide2.jpg" style="width:100%">
-          <div class="text">Lorem ispum 2</div>
-        </div>
-        
-        <div class="mySlides fade">
-          <div class="numbertext">3 / 3</div>
-          <img class="imgcarrosel" src="./imagens/slide3.jpg" style="width:100%">
-          <div class="text">Lorem ipsum 3</div>
+          <div class="text">Noticias </div>
         </div>
         
         <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -67,25 +49,8 @@
     <footer>
       <div class="footer-container">
         <ul>
-          <h2>TITULO</h2>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-        </ul>
-        <ul>
-          <h2>TITULO</h2>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-        </ul>
-        <ul>
-          <h2>TITULO</h2>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
-          <li>Lorem ipsum dolor</li>
+          <h2>Blog Sailor</h2>
+          <li>Noticias e conteudos incriveis todos os dias.</li>
         </ul>
       </div>
     </footer>
